d36ee54d42d72b14d1206399ac800c6f4914974e
Sparkle1881 <124931392+Sparkle1881@users.noreply.github.com>
2026-09-16 15:01:11 -0500

    Back in stable state with new branch for implementing features before master branch

--- src/frontend/components/ui/Ui.tsx
+++ src/frontend/components/ui/Ui.tsx
@@ -1,9 +1,14 @@
+import { SearchBar } from "./SearchBar";
 import './ui.css';
 
 export function Ui() {
     return (
         <section id="ui">
             <div id="backgroundBar"></div>
+            <h1 id="homeButton">Home</h1>
+            <h1 id="profileButton">Profile</h1>
+            <h1 id="settingsButton">Settings</h1>
+            <SearchBar></SearchBar>
         </section>
     )
 }
